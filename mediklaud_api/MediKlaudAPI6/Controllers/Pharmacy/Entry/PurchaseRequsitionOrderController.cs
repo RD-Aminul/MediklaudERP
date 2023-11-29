117de1f82d74601eec14c1d43c690e4ec79ac951
Aminul Islam <mdaminulislam2424@gmail.com>
2023-11-29 20:07:47 +0600

    commite for update

---
+++ mediklaud_api/MediKlaudAPI6/Controllers/Pharmacy/Entry/PurchaseRequsitionOrderController.cs
@@ -0,0 +1,53 @@
+﻿using mediklaud_api.Interface.Pharmacy.Entry;
+using mediklaud_api.Models.Pharmacy.Entry;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mediklaud_api.Controllers.Pharmacy.Entry
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PurchaseRequsitionOrderController : Controller
+    {
+        private readonly IPurchaseRequsitionOrderService _purchaseRequsitionOrder;
+        public PurchaseRequsitionOrderController(IPurchaseRequsitionOrderService purchaseRequsitionOrder)
+        {
+            _purchaseRequsitionOrder = purchaseRequsitionOrder;
+        }
+
+        [HttpGet("GetSerStoreList")]
+        public async Task<string> GetSerStoreList([FromQuery] PurReqOrderStoreList purReqOrderStoreList)
+        {
+            return await _purchaseRequsitionOrder.GetSerStoreList(purReqOrderStoreList);
+        }
+
+        [HttpGet("GetStoreList")]
+        public async Task<string> GetStoreList([FromQuery] PurReqOrderStoreList purReqOrderStoreList)
+        {
+            return await _purchaseRequsitionOrder.GetStoreList(purReqOrderStoreList);
+        }
+
+        [HttpGet("GetForStoreList")]
+        public async Task<string> GetForStoreList([FromQuery] PurReqOrderStoreList purReqOrderStoreList)
+        {
+            return await _purchaseRequsitionOrder.GetForStoreList(purReqOrderStoreList);
+        }
+
+        [HttpGet("GetSerSupplierList")]
+        public async Task<string> GetSerSupplierList([FromQuery] CommonGID_CID_For_PurReqOrd commonGID_CID_For_PurReqOrd)
+        {
+            return await _purchaseRequsitionOrder.GetSerSupplierList(commonGID_CID_For_PurReqOrd);
+        }
+
+        [HttpGet("GetSupplierList")]
+        public async Task<string> GetSupplierList([FromQuery] CommonGID_CID_For_PurReqOrd commonGID_CID_For_PurReqOrd)
+        {
+            return await _purchaseRequsitionOrder.GetSupplierList(commonGID_CID_For_PurReqOrd);
+        }
+
+        [HttpGet("GetTransactionList")]
+        public async Task<string> GetTransactionList([FromQuery] TransactionListForPhrReqOrd transactionListForPhrReqOrd)
+        {
+            return await _purchaseRequsitionOrder.GetTransactionList(transactionListForPhrReqOrd);
+        }
+    }
+}
---
+++ mediklaud_api/MediKlaudAPI6/Interface/Pharmacy/Entry/IPurchaseRequsitionOrderService.cs
@@ -0,0 +1,14 @@
+﻿using mediklaud_api.Models.Pharmacy.Entry;
+
+namespace mediklaud_api.Interface.Pharmacy.Entry
+{
+    public interface IPurchaseRequsitionOrderService
+    {
+        Task<string> GetSerStoreList(PurReqOrderStoreList purReqOrderStoreList);
+        Task<string> GetStoreList(PurReqOrderStoreList purReqOrderStoreList);
+        Task<string> GetForStoreList(PurReqOrderStoreList purReqOrderStoreList);
+        Task<string> GetSerSupplierList(CommonGID_CID_For_PurReqOrd commonGID_CID_For_PurReqOrd);
+        Task<string> GetSupplierList(CommonGID_CID_For_PurReqOrd commonGID_CID_For_PurReqOrd);
+        Task<string> GetTransactionList(TransactionListForPhrReqOrd transactionListForPhrReqOrd);
+    }
+}
